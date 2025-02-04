1810108f3bdb2c20cdc3253eb7d62b5130c628e1
pneumatick <s@urn>
2025-02-04 11:26:20 -0500

    Return the hash and nonce as String and usize respectively so that the JS front-end can use them

--- src/lib.rs
+++ src/lib.rs
@@ -3,36 +3,69 @@ use std::sync::{atomic::{AtomicBool, Ordering}, Arc};
 use sha2::{ Digest, Sha256 };
 use rayon::prelude::*;
 
-
-//const MAX_THREADS: usize = 8;
-
+// Expose alert to JS (mainly used for debugging)
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
+// For relaying the mining result back to JS
 #[wasm_bindgen]
-pub fn init_mine(data: &str, target: &str, difficulty: usize, num_threads: usize) {
-    // Hashing 
-    //let threads: Vec<usize> = (0..MAX_THREADS).collect();
+pub struct MineResult {
+    hash: String,
+    nonce: usize,
+}
+
+#[wasm_bindgen]
+impl MineResult {
+    #[wasm_bindgen(getter)]
+    pub fn hash(&self) -> String {
+        self.hash.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn nonce(&self) -> usize {
+        self.nonce
+    }
+}
+
+// Initialize the mining process (JS-facing function)
+#[wasm_bindgen]
+pub fn init_mine(data: &str, target: &str, difficulty: usize, num_threads: usize) -> MineResult {
     let threads: Vec<usize> = (0..num_threads).collect();
     let found = Arc::new(AtomicBool::new(false));
-    threads.into_par_iter().for_each(|thread| {
+    
+    // Parallelize the mining process
+    let result = threads.into_par_iter().map(|thread| {
         let found = Arc::clone(&found);
         match mine(data.as_bytes(), &target, difficulty, num_threads, thread, &found) {
             Ok((hash, nonce)) => {
                 if !found.load(Ordering::Relaxed) {
-                    alert(&format!("Hash: {:?}, Nonce: {:?}", hash, nonce));
+                    found.store(true, Ordering::Relaxed);
+                    Some(MineResult { hash, nonce })
                 }
-                found.store(true, Ordering::Relaxed);
+                else {
+                    None
+                }
             },
             Err(e) => {
                 alert(&format!("Error: {:?}", e));
+                None
             }
         }
     });
+
+    // Return the found result or a default empty result when errors occur
+    match result.find_map_any(|x| x) {
+        Some(result) => result,
+        None => MineResult {
+            hash: "".to_string(),
+            nonce: 0,
+        }
+    }
 }
 
+// Thread-bound mining function
 fn mine(data: &[u8], target: &str, difficulty: usize, 
     num_threads: usize, start: usize, found: &Arc<AtomicBool>) 
     -> Result<(String, usize), Box<dyn std::error::Error>> 
@@ -43,6 +76,7 @@ fn mine(data: &[u8], target: &str, difficulty: usize,
     let mut nonce: usize = start;
     let target = target.to_owned() + &"0".repeat(difficulty);
 
+    // Mining loop
     while hex::encode(&hash)
     .chars()
     .take(target.len())
